5d05d4f80e985d650c418ac36c20e2024a0c2f50
Rakesh3117 <annavarapurakesh190799@gmail.com>
2024-11-16 16:48:28 +0530

    Initial Commit

---
+++ Backend/Models/Contacts.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Contacts = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("contactsData", Contacts);
